ea1109bce8d9f6ac96ba6f39c702e9b9a53f8723
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-09 11:27:31 +0600

    Create Combine Scores from Two Exams.py

---
+++ Combine Scores from Two Exams.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Get input for exam1 scores
+exam1_scores = list(map(int, input().split()))
+
+# Get input for exam2 scores
+exam2_scores = list(map(int, input().split()))
+
+# Convert lists to numpy arrays
+
+
+# Add the two arrays to get total scores
+total_scores = np.add(exam2_scores,exam1_scores)# Your code here
+
+# Print the total scores
+print(total_scores)
